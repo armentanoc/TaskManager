668d0fe86a22d9bbc78b32377878c3e85111974a
armentanoc <armentanocarolina@gmail.com>
2023-12-15 15:04:28 -0300

    Add project files.

---
+++ TaskManager.Core/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManager.Core
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
