ac7401ce81306783181fc66aa5ceeafcaeb87261
armentanoc <armentanocarolina@gmail.com>
2023-12-30 10:04:13 -0300

    feat: dev task status enum.

---
+++ TaskManager.DomainLayer/Model/Tasks/StatusEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskManager.DomainLayer.Model.Tasks
+{
+    public enum StatusEnum
+    {
+        EmAnaliseParaBacklog = 1,
+        Backlog = 2,
+        EmProgresso = 3,
+        Atrasada = 4,
+        EmAnaliseParaConclusao = 5,
+        Concluida = 6
+    }
+}
