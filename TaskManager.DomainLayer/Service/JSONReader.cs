78b7dd7ee7833bff4249f3fc50d41a03a74cac82
armentanoc <armentanocarolina@gmail.com>
2023-12-30 08:08:36 -0300

    Refactor: putting the JSON Reader in a separate class.

---
+++ TaskManager.DomainLayer/Service/JSONReader.cs
@@ -0,0 +1,57 @@
+﻿
+using System.Text.Json;
+using TaskManager.ConsoleInteraction.Components;
+using TaskManager.DomainLayer.DTO;
+using TaskManager.DomainLayer.Model;
+
+namespace TaskManager.DomainLayer.Service
+{
+    internal class JSONReader
+    {
+        public static List<User> Execute(List<User> userList, string fullPath)
+        {
+            string jsonData = File.ReadAllText(fullPath);
+            var userDTOs = JsonSerializer.Deserialize<List<UserDTO>>(jsonData);
+
+            foreach (var userDTO in userDTOs)
+            {
+                User existingUser = userList.FirstOrDefault(u => u.Name == userDTO.Name || u.Login == userDTO.Login);
+
+                if (existingUser != null)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"\nAtenção: Usuário com nome '{userDTO.Name}' ou login '{userDTO.Login}' já existe. Pulando.");
+                    Console.ResetColor();
+                    continue;
+                }
+
+                User user;
+
+                if (!Enum.TryParse(userDTO.Job, out JobEnum job))
+                {
+                    Console.WriteLine($"Tipo de trabalho inválido: {userDTO.Job}");
+                    continue;
+                }
+
+                switch (userDTO.Job)
+                {
+                    case "Developer":
+                        user = new Developer(userDTO.Name, userDTO.Login, userDTO.Email);
+                        break;
+
+                    case "TechLeader":
+                        user = new TechLeader(userDTO.Name, userDTO.Login, userDTO.Email);
+                        break;
+
+                    default:
+                        Console.WriteLine($"Tipo de usuário desconhecido: {userDTO.Job}");
+                        continue;
+                }
+                user.SetJob(job);
+                userList.Add(user);
+            }
+            Message.NewUsersInUserList();
+            return userList;
+        }
+    }
+}
