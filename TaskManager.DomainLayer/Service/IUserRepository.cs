1d2728033d3e5e0a1b7859b1b661dd330e7efecb
armentanoc <armentanocarolina@gmail.com>
2024-01-02 15:46:37 -0300

    feat: interface to communicate with user repository.

---
+++ TaskManager.DomainLayer/Service/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.DomainLayer.Model.People;
+
+namespace TaskManager.DomainLayer.Service
+{
+    public interface IUserRepository
+    {
+        User GetUserByLogin(string login);
+    }
+}
